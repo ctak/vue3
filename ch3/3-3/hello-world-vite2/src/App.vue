9e94c8977437099f2803769342d103bb0cf61245
tak <tak.changbeom@gmail.com>
2022-02-02 16:49:56 +0900

    Vite Alias 생성

--- ch3/3-3/hello-world-vite2/src/App.vue
+++ ch3/3-3/hello-world-vite2/src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '/@components/HelloWorld.vue'
 </script>
 
+/*
+// vscode 에서 아무리 빨간색이라도 vue3 에서는 template 의 ROOT 는 필요없어.
+*/
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
