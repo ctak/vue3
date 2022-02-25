c030b44abce353050f09e936191d47ab6fa06838
tak <tak.changbeom@gmail.com>
2022-02-25 22:25:50 +0900

    non-props 그리고 custom-event

---
+++ ch3/3-4/custom-event/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Component from './components/Component.vue';
+
+export default {
+  data() {
+    return {
+      msg: '초기 메시지',
+      msg2: '초기 메시지 2',
+    }
+  },
+  components: {
+    Component,
+  }
+}
+</script>
+
+<template>
+  <p>{{ `[${msg}] : ${msg2}` }}</p>
+  <Component v-model:msg="msg" v-model:msg2="msg2" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ch3/3-4/non-props/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+module.exports = {
+  alias: {
+    vue: 'vue/dist/vue.esm-bundler.js',
+  },
+}
